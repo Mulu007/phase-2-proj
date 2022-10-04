7738fd3e397058c2a32c73af93cdabe823ae8f78
Mulu <brianmulu007@gmail.com>
2022-10-04 10:43:09 +0300

    Customization of screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ const Navbar = () => {
   return (
     <div className='fixed w-full text-white flex justify-between p-4 items-center'>
 
-      <div className="text-2xl font-bold text-center">
-        <h1>e <span className='block uppercase text-4xl'>RARE</span></h1>
+      <div className="text-2xl font-bold text-center uppercase">
+        <h1>e <span className='block text-4xl'>RARE</span></h1>
       </div>
 
       <nav>
 
-        <ul>
+        <ul className='md:flex'>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/">About</Link></li>
             <li><Link to="/">Contact</Link></li>
