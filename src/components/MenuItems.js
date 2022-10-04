5647ec28737547a3d04c01c19f5db9d792e19cc3
Mulu <brianmulu007@gmail.com>
2022-10-04 11:24:13 +0300

    Addition of close icon

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -5,6 +5,9 @@ const MenuItems = () => {
   return (
     <div>
        <ul className=''>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/">About</Link></li>
             <li><Link to="/">Contact</Link></li>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,8 +18,8 @@ const Navbar = () => {
       </div>
 
       <nav>
-           <div className=''>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+           <div >
+                <svg onClick= {showMenu} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
             </div> 
