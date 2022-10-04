5a718ceaf45a59c457aca5248a4688ddac351494
Mulu <brianmulu007@gmail.com>
2022-10-04 11:41:36 +0300

    Addition of styling in navbar child ul

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MenuItems from './MenuItems'
 import { Link } from 'react-router-dom'
 
@@ -18,7 +18,7 @@ const Navbar = () => {
       </div>
 
       <nav>
-           <div className=''>
+           <div className='absolute left-1'>
                 <svg onClick= {showMenu} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
