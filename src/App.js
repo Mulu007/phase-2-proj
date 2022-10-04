99e3eb6bf71f473b44a3e7156db906ae727ccf8e
Mulu <brianmulu007@gmail.com>
2022-10-04 13:44:28 +0300

    Addition of files in the components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import React from "react";
 import Navbar from "./components/Navbar";
 import Section from "./components/Section";
+import {Routes, Route} from "react-router-dom"
+import About from "./components/About"
+
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -8,11 +8,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
