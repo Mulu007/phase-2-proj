a1a4572c65bdc29c0becaecf14080fa3f3d83071
Mulu <brianmulu007@gmail.com>
2022-10-04 09:55:46 +0300

    Addition of components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Section from "./components/Section";
+
 function App() {
   return (
     <div className="text-red-500">
-      hello
+      <Section />
     </div>
   );
 }
---
+++ src/components/Section.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <div>
+      <div>
+        <h1>RARE COLLECTION</h1>
+        <h3>Be your own label</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Section
