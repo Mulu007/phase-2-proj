80e91f9ec68955a00dc1965bc30115af4ba23cf1
Mulu <brianmulu007@gmail.com>
2022-10-04 10:29:06 +0300

    Addition of links to NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar";
 import Section from "./components/Section";
 
 function App() {
   return (
     <div className="text-red-500">
-      <Header />
+      <Navbar />
       <Section />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,25 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='fixed'>
+
       <div className="logo">
         <h1>e <span>RARE</span></h1>
       </div>
+
+      <nav>
+
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">About</Link></li>
+            <li><Link to="/">Contact</Link></li>
+            <li><Link to="/">Products</Link></li>
+            <li><Link to="/">Reviews</Link></li>
+        </ul>
+
+
+
+      </nav>
     </div>
   )
 }
