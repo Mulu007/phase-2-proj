4df69645fb812f12bf96f7ab9886da38630a323d
Mulu <brianmulu007@gmail.com>
2022-10-04 10:22:47 +0300

    Adding header component to parent directory

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Section from "./components/Section";
 function App() {
   return (
     <div className="text-red-500">
+      <Header />
       <Section />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      
+      <div className="logo">
+        <h1>e <span>RARE</span></h1>
+      </div>
     </div>
   )
 }
