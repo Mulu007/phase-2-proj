9448fb92dd299c2ed39bc4c117ac55d14b0eb79f
Mulu <brianmulu007@gmail.com>
2022-10-04 19:09:23 +0300

    Addition of image containers

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import ProductsCard from './ProductsCard';
 
 const Products = () => {
   const [images,setImages] = useState([]);
@@ -17,28 +18,11 @@ const Products = () => {
   },[])
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by Silvanius
-        </div>
-        <ul>
-          <li>
-            <strong>Views:</strong>4000
-          </li>
-          <li>
-            <strong>Downloads:</strong>300
-          </li>
-          <li>
-            <strong>Likes:</strong>100
-          </li>
-        </ul>
-      </div>
-      <div className="px=6 py-4 ">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag1</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag2</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#tag3</span>
+    <div className='container-mx-auto'>
+      <div className='grid grid-cols-3 gap-4'>
+        {images.map(image => (
+          <ProductsCard key={image.id} image={image}/>
+        ))}
       </div>
     </div>
   );
