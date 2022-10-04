771c5cb525c4d9c8c34e62731498071896d9ae05
Mulu <brianmulu007@gmail.com>
2022-10-04 10:18:27 +0300

    Addition of styling to the section

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Section = () => {
   return (
-    <div className='bg-hero-image'>
-      <div>
-        <h1>RARE COLLECTION</h1>
+    <div className='bg-hero-image w-full h-screen bg-cover bg-center flex items-center px-4'>
+      <div className="uppercase text-center text-white font-extrabold">
+        <h1 className='text-8xl'>RARE COLLECTION</h1>
         <h3>Be your own label</h3>
       </div>
     </div>
