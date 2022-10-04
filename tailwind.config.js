881130095499412fe20770b1b9f53f02bcc91417
Mulu <brianmulu007@gmail.com>
2022-10-04 10:06:02 +0300

    Addition of fashion.jpg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'hero-image':'url("./components./assets/background.webp")'
+      },
+    },
   },
   plugins: [],
 };
