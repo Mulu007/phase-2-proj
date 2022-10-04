ed6e5601711cba0311283b9cd8cac042c504a9d3
Mulu <brianmulu007@gmail.com>
2022-10-04 10:09:54 +0300

    Addition of background image to tailwing configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage:{
-        'hero-image':'url("./components./assets/background.webp")'
+      backgroundImage: {
+        "hero-image": 'url("./components/assets/fashion.jpg")',
       },
     },
   },
