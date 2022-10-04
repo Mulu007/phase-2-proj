447ef70541502113b8f6ada05531200dd2daabc9
Mulu <brianmulu007@gmail.com>
2022-10-04 19:16:14 +0300

    Data fetching

--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const ProductsCard = () => {
+const ProductsCard = ({image}) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <img src={image.image} alt="" className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by Silvanius
+          Photo by {image.condition}
         </div>
         <ul>
           <li>
