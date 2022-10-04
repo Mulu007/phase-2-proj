c75aa6356371342383dacefa9c296d42cd83b4e7
Mulu <brianmulu007@gmail.com>
2022-10-04 10:49:48 +0300

    Addition of links into menuitems

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -3,7 +3,13 @@ import React from 'react'
 const MenuItems = () => {
   return (
     <div>
-      
+       <ul className=''>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">About</Link></li>
+            <li><Link to="/">Contact</Link></li>
+            <li><Link to="/">Products</Link></li>
+            <li><Link to="/">Reviews</Link></li>
+        </ul>
     </div>
   )
 }
