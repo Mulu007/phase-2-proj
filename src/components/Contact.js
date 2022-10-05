96d8d573c92bf94aced86687ee1829f7bb4ba8b9
Mulu <brianmulu007@gmail.com>
2022-10-05 12:29:03 +0300

    Adding onSubmit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,66 +1,6 @@
 import React, { useState } from 'react'
 
 const Contact = () => {
-  const [name, setName] = useState([])
-  const [email, setEmail] = useState([])
-  const [subject, setSubject] = useState([])
-  const [message, setMessage] = useState([])
-  const [submittedData, setSubmittedData] = useState([])
-
-  function handleNameChange(event) {
-    setName(event.target.value)
-  }
-
-  function handleEmailChange(event) {
-    setEmail(event.target.value)
-  }
-
-  function handleSubjectChange(event) {
-    setSubject(event.target.value)
-  }
-
-  function handleMessageChange(event) {
-    setMessage(event.target.value);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-  
-  const [errors, setErrors] = useState([]);
-
-    if (name.length > 0) {
-      const formData = {name: name, email: email, subject: subject, message: message}
-      const dataArray = [...submittedData, formData]
-      setSubmittedData(dataArray)
-      setName("")
-      setEmail("")
-      setMessage("")
-      setSubject("")
-      setErrors([])
-    } else {
-      setErrors(["Name is required!"])
-    }
-  }
-
-  const listOfSubmissions = submittedData.map((data, index) => {
-    return (
-      <div key={index}>
-        {data.name} {data.email} {data.subject} {data.message}
-      </div>
-    )
-  })
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleNameChange} value={name} />
-        <input type="text" onChange={handleNameChange} value={name} />
-        <input type="text" onChange={handleNameChange} value={name} />
-        <input type="text" onChange={handleNameChange} value={name} />
-      </form>
-    </div>
-  );
-  
 
   return (
     <div >
@@ -71,4 +11,4 @@ const Contact = () => {
   )
 }
 
-export default Contact
+export default Contact;
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Reviews = () => {
+  const [name, setName] = useState([]);
+  const [email, setEmail] = useState([]);
+  const [message, setMessage] = useState([]);
+  const [submittedData, setSubmittedData] = useState([]);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  function handleMessageChange(event) {
+    setEmail(event.target.value);
+  }
+
+  // add state for holding error messages
+  const [errors, setErrors] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // first name is required
+    if (name.length > 0) {
+      const formData = { name: name, email: email, message: message};
+      const dataArray = [...submittedData, formData];
+      setSubmittedData(dataArray);
+      setName("");
+      setEmail("");
+      setMessage("");
+      setErrors([]);
+    } else {
+      setErrors(["Name is required!"]);
+    }
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.name} {data.email} {data.message}
+      </div>
+    );
+  });
+
   return (
-    <div>
-      <h1>Reviews</h1>
+    <div >
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleNameChange} value={name} />
+        <input type="text" onChange={handleEmailChange} value={email} />
+        <input type="text" onChange={handleMessageChange} value={message} />
+        <button type="submit">Submit</button>
+      </form>
+      {/* conditionally render error messages */}
+      {errors.length > 0
+        ? errors.map((error, index) => (
+            <p key={index} style={{ color: "red" }}>
+              {error}
+            </p>
+          ))
+        : null}
+      <h3>Reviews</h3>
+      {listOfSubmissions}
     </div>
-  )
-}
+  );
+};
+
+//   return (
+//     <div>
+//       <h1>Reviews</h1>
+//     </div>
+//   )
+// }
 
 export default Reviews
