a15de6e6c88dcac58ef104d2d2b756144f92674f
Mulu <brianmulu007@gmail.com>
2022-10-05 08:06:06 +0300

    Deletionn of map

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      <h1><span>Contact</span>Us</h1>
-      <div>
-        <iframe className="w-100 h=100" src="https://www.google.com/maps/d/embed?mid=1SuwNJ-HqTWSF2Bmaur7HqmVqZBk&ie=UTF8&hl=en&msa=0&ll=-1.2897330000000091%2C36.80705299999999&spn=0.008581%2C0.010707&z=16&iwloc=0004817447742f770a649&output=embed" width={1000} height={500}></iframe>
+    <div >
+      <h1 className='text-3xl text-center font-extrabold '>Contact</h1>
+      <div className='container'>
+        {/* <iframe src="https://www.google.com/maps/d/embed?mid=1SuwNJ-HqTWSF2Bmaur7HqmVqZBk&ie=UTF8&hl=en&msa=0&ll=-1.2897330000000091%2C36.80705299999999&spn=0.008581%2C0.010707&z=16&iwloc=0004817447742f770a649&output=embed" width={500} height={500}></iframe> */}
       </div>
       
     </div>
