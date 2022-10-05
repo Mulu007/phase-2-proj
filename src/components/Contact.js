662e4abed3bd75ac18c7347a411e750a9daf3608
Mulu <brianmulu007@gmail.com>
2022-10-05 11:56:27 +0300

    Addition of functions in contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,29 @@ import React, { useState } from 'react'
 
 const Contact = () => {
   const [name, setName] = useState([])
+  const [email, setEmail] = useState([])
+  const [subject, setSubject] = useState([])
+  const [message, setMessage] = useState([])
   const [submittedData, setSubmittedData] = useState([])
-  
+
+  function handleNameChange(event) {
+    setName(event.target.value)
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value)
+  }
+
+  function handleSubjectChange(event) {
+    setSubject(event.target.value)
+  }
+
+  function handleMessageChange(event) {
+    setMessage(event.target.value);
+  }
+
+  const [errors, setErrors] = useState([])
+
   return (
     <div >
       <h1 className='text-3xl text-center font-extrabold '>Contact</h1>
