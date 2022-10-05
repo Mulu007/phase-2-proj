df393ebe35859651baf28b708a99f29b413cc300
Mulu <brianmulu007@gmail.com>
2022-10-05 11:44:58 +0300

    Addition of paragraphs in about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,27 +4,35 @@ const About = () => {
   return (
     <div>
       <h1 className="text-3xl text-center font-extrabold ">About</h1>
-      
-      <div className='container-mx-auto'>
-        
-      </div>
-      
-      <p className='text-right'>
-        Rare's clean design and simple functionality make the experience
-        about the product in a way that's inclusive and not intimidating. The
-        three categories — Rare (highly sought after pieces from well known
-        designers and labels), hype (streetwear and new releases), and basics
-        (vintage and mass market brands) — are easy to navigate, whether you're
-        looking for something specific or just browsing. The labels for sale
-        speak for themselves: Supreme, Nike, and Palace sit next to vintage Raf
-        Simons, Helmut Lang, and Number (N)ine. Reselling here isn't just for
-        the thrifty — it allows fashion enthusiasts to shop more and experiment
-        with new looks and designers, within a community of like-minded people.
-        Before Rare Collection, these brands were being resold amongst devout followers,
-        but on nerdy, hard-to-decipher forums and eBay pages unearthed only
-        through deep keyword searches. Rare Collection has taken this practice,
-        streamlined it for the diehard shopper, and made it accessible to the
-        more mainstream, casual consumer.
+      <img src="/assets/runway1.jpg" alt="" />
+      <div></div>
+
+      <p className="text-right">
+        Rare's clean design and simple functionality make the experience about
+        the product in a way that's inclusive and not intimidating. The three
+        categories — Rare (highly sought after pieces from well known designers
+        and labels), hype (streetwear and new releases), and basics (vintage and
+        mass market brands) — are easy to navigate, whether you're looking for
+        something specific or just browsing. The labels for sale speak for
+        themselves: Supreme, Nike, and Palace sit next to vintage Raf Simons,
+        Helmut Lang, and Number (N)ine. Reselling here isn't just for the
+        thrifty — it allows fashion enthusiasts to shop more and experiment with
+        new looks and designers, within a community of like-minded people.
+        Before Rare Collection, these brands were being resold amongst devout
+        followers, but on nerdy, hard-to-decipher forums and eBay pages
+        unearthed only through deep keyword searches. Rare Collection has taken
+        this practice, streamlined it for the diehard shopper, and made it
+        accessible to the more mainstream, casual consumer.
+      </p>
+      <p>
+        Rare Collection then hosts modelling events exhibiting their products.
+        Modelling is considered to be different
+        from other types of public performance, such as acting or dancing.
+        Although the difference between modelling and performing is not always
+        clear, appearing in a film or a play is not generally considered to be
+        "modelling". 
+        Personal opinions are generally not expressed and a model's reputation
+        and image are considered critical.
       </p>
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     <div >
       <h1 className='text-3xl text-center font-extrabold '>Contact</h1>
       <div className='container'>
-          <form className=''></form>
+          
       </div>
       
     </div>
