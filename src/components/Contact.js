f3f718eec263c04b1585bbd6cc4d7b04d2673006
Mulu <brianmulu007@gmail.com>
2022-10-05 07:50:28 +0300

    Addition of map

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <h1><span>Contact</span>Us</h1>
+      <div>
+        <iframe className="w-100 h=100" src="https://www.google.com/maps/d/embed?mid=1SuwNJ-HqTWSF2Bmaur7HqmVqZBk&ie=UTF8&hl=en&msa=0&ll=-1.2897330000000091%2C36.80705299999999&spn=0.008581%2C0.010707&z=16&iwloc=0004817447742f770a649&output=embed" width={1000} height={500}></iframe>
+      </div>
+      
     </div>
   )
 }
